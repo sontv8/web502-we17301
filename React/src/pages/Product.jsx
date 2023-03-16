3677ffece904f7229bd1293905b8dddb6727d157
sontv8 <sontv8@fpt.edu.vn>
2023-03-16 13:55:28 +0700

    Buoi 5: remove product

--- React/src/pages/Product.jsx
+++ React/src/pages/Product.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
 const ProductPage = (props) => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        setData(props.products)
+    }, [props])
+    const removeProduct = (id) => {
+        fetch('http://localhost:3000/products/' + id, {
+            method: 'DELETE'
+        }).then(() => setData(data.filter(item => item.id !== id)))
+    }
     return (
         <div>
             <h1>ProductPage</h1>
-            {props.products.map(product => {
+            {data.map(product => {
                 return <div key={product.id}>
                     <h2>{product.name}</h2>
-                    <button>Remove</button>
+                    <button onClick={() => removeProduct(product.id)}>Remove</button>
                 </div>
             })
             }
