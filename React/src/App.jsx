6aaf10e8a70a769d17fda64440ca8c95d2f6e1e3
sontv8 <sontv8@fpt.edu.vn>
2023-03-16 12:29:01 +0700

    Buoi 5: React - router

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import HomePage from './HomePage'
+import HomePage from './pages/HomePage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-      <div><HomePage /></div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+
     </div >
   )
 }
