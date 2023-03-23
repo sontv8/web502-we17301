cbc7fdf8cc666ae8cae71d6fe3348a2d0b6ae7c2
sontv8 <sontv8@fpt.edu.vn>
2023-03-23 12:23:46 +0700

    Buổi 6: axios - truyền dữ liệu giữa các component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import ProductPage from './pages/Product'
 import ProductDetailPage from './pages/ProductDetail'
+import { deleteProduct } from './api/product'
 
 function App() {
   const [products, setProducts] = useState([])
@@ -13,11 +14,17 @@ function App() {
       .then(response => response.json())
       .then(data => setProducts(data))
   }, [])
+  const onHandleRemove = (id) => {
+    // fetch('http://localhost:3000/products/' + id, {
+    //   method: 'DELETE'
+    // }).then(() => setProducts(products.filter(item => item.id !== id)))
+    deleteProduct(id).then(() => setProducts(products.filter(item => item.id !== id)))
+  }
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/products' element={<ProductPage products={products} />} />
+        <Route path='/products' element={<ProductPage products={products} onRemove={onHandleRemove} />} />
         <Route path='/products/:id' element={<ProductDetailPage />} />
       </Routes>
 
---
+++ React/src/api/product.jsx
@@ -0,0 +1,19 @@
+import instance from "./instance";
+
+const getAllProduct = () => {
+    return instance.get('/products')
+}
+const getOneProduct = (id) => {
+    return instance.get('/products/' + id)
+}
+const addProduct = (product) => {
+    return instance.post('/products', product)
+}
+const deleteProduct = (id) => {
+    return instance.delete('/products/' + id)
+}
+const updateProduct = (product) => {
+    return instance.put('/products/' + product.id, product)
+}
+
+export { getAllProduct, getOneProduct, addProduct, deleteProduct, updateProduct }
