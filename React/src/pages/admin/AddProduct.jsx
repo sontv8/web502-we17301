836a741d5db4acca40e89f3b6d59216273f15640
sontv8 <sontv8@fpt.edu.vn>
2023-03-23 13:31:09 +0700

    Buổi 7: thêm mới sản phẩm - xử lý nhiều input

--- React/src/pages/admin/AddProduct.jsx
+++ React/src/pages/admin/AddProduct.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddProductPage = (props) => {
-    const [inputValue, setInputValue] = useState({})
-    const onHandleChange = (e) => {
-        setInputValue({ name: e.target.value });
+    const navigate = useNavigate() // khởi tạo navigate để điều hướng
+    const [inputValue, setInputValue] = useState({}) // khởi tạo state để lưu giá trị input
+    const onHandleChange = (e) => { // hàm xử lý sự kiện khi input thay đổi
+        // const name = e.target.name; // lấy name của input
+        // const value = e.target.value// lấy value của input
+        const { name, value } = e.target
+        setInputValue({ ...inputValue, [name]: value }); // gán giá trị cho state
+
+        // setInputValue({ name: e.target.value });
     }
-    const onHandleSubmit = (e) => {
+    const onHandleSubmit = (e) => { // hàm xử lý sự kiện khi submit form
         e.preventDefault()
-        props.onAdd(inputValue);
+        // console.log(inputValue);
+        props.onAdd(inputValue); // gọi hàm onAdd từ props để thêm sản phẩm
+        navigate('/admin/products')// điều hướng đến trang quản lý sản phẩm
     }
     return (
         <div>
             <form action="" onSubmit={onHandleSubmit}>
-                <input type="text" placeholder='Product Name' onChange={onHandleChange} />
+                <input type="text" placeholder='Product Name' onChange={onHandleChange} name='name' />
+                <input type="number" onChange={onHandleChange} name='price' />
                 <button type="submit">Add New Product</button>
             </form>
         </div>
